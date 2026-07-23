b7d03af51b8332e30b9a9edb23256e2b814863dc
a01097119441-pixel <a01097119441@gmail.com>
2026-07-23 15:48:48 +0900

    Update app.py

--- movie/app.py
+++ movie/app.py
@@ -12,7 +12,7 @@ movies = {
         "genre": "애니메이션",
         "story": "라일리의 머릿속에는 기쁨, 슬픔, 버럭, 까칠, 소심이라는 다섯 감정이 살고 있습니다. 새로운 환경에서 여러 감정을 겪으며 진정한 행복과 성장의 의미를 깨닫는 따뜻한 감동 애니메이션입니다.",
         "color": "#FFE066",
-        "poster": "0BiHlYxIl2KJ0KvR3PRDXvBrZ497r7sVbGbsFnX8NzcBiNqI_SERAonKt1DrX4YplofclKEk4d1Mg_KkBBqOa45nA5sFJH6-9XkaNvfimin6P5UfiuBtaOwaULUI1xHP3ld_j-7VCANkHlaSAQkhxQ"
+        "poster": '0BiHlYxIl2KJ0KvR3PRDXvBrZ497r7sVbGbsFnX8NzcBiNqI_SERAonKt1DrX4YplofclKEk4d1Mg_KkBBqOa45nA5sFJH6-9XkaNvfimin6P5UfiuBtaOwaULUI1xHP3ld_j-7VCANkHlaSAQkhxQ'
     },
 
     "😢 슬픔": {
@@ -20,7 +20,7 @@ movies = {
         "genre": "애니메이션",
         "story": "음악을 사랑하는 소년 미구엘이 죽은 자들의 세상으로 떠나며 가족의 사랑과 추억의 소중함을 깨닫는 감동적인 이야기입니다.",
         "color": "#74C0FC",
-        "poster": "8290_8563_418"
+        "poster": '8290_8563_418'
     },
 
     "😍 설렘": {
@@ -28,7 +28,7 @@ movies = {
         "genre": "로맨스",
         "story": "배우를 꿈꾸는 미아와 재즈 피아니스트 세바스찬이 꿈과 사랑 사이에서 고민하며 성장하는 아름다운 뮤지컬 영화입니다.",
         "color": "#FFB3C6",
-        "poster": "MHgGkv3aInGRnLq5bnY7tO834TratDtR-h3l2Ci05sCci6KWeLRuo1qB1YTjMHq3-nVv12wXCwH_D409fVFHfm-TMANWg4OOdK_EJgJX4eAma8CDI7rqgyC6MHzdMRvacmUVapz9occV4-uKm0jAAA"
+        "poster": 'MHgGkv3aInGRnLq5bnY7tO834TratDtR-h3l2Ci05sCci6KWeLRuo1qB1YTjMHq3-nVv12wXCwH_D409fVFHfm-TMANWg4OOdK_EJgJX4eAma8CDI7rqgyC6MHzdMRvacmUVapz9occV4-uKm0jAAA'
     },
 
     "😎 액션": {
@@ -36,7 +36,7 @@ movies = {
         "genre": "액션",
         "story": "전설적인 파일럿 매버릭이 젊은 조종사들과 함께 위험한 임무를 수행하며 최고의 비행 액션을 선보입니다.",
         "color": "#FF922B",
-        "poster": "다운로드"
+        "poster": '다운로드'
     },
 
     "😂 웃고 싶다": {
@@ -44,7 +44,7 @@ movies = {
         "genre": "코미디",
         "story": "범인을 잡기 위해 치킨집을 운영하게 된 형사들이 뜻밖의 대박을 맞으며 벌어지는 유쾌한 코미디 영화입니다.",
         "color": "#69DB7C",
-        "poster": "다운로드 (1)"
+        "poster": '다운로드 (1)'
     },
 
     "😱 무섭다": {
@@ -52,7 +52,7 @@ movies = {
         "genre": "공포",
         "story": "유명한 폐병원 곤지암을 실시간 방송으로 탐험하던 사람들이 설명할 수 없는 공포를 마주하게 되는 한국 공포 영화입니다.",
         "color": "#845EF7",
-        "poster": "132246172.1"
+        "poster": '132246172.1'
     }
 }
 
